ccc837df1681aecc48cfb8cb17e80a49838c2163
Abdullah Essam <b21ahashim@gmail.com>
2024-04-27 02:22:03 +0300

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,12 @@ import prisma from "@/lib/db/prisma";
 import { Task } from "@prisma/client";
 import { Suspense } from "react";
 
-const dynamic = "force-dynamic";
+async function getTasks() {
+  const data = await prisma.task.findMany({
+    orderBy: { id: "desc" },
+  });
+  return data;
+}
 
 async function Home() {
   // const res = await fetch(process.env.URL + "/api/tasks", {
@@ -13,9 +18,7 @@ async function Home() {
   // console.log(res);
   // const tasks: Task[] = await res.json();
 
-  const tasks = await prisma.task.findMany({
-    orderBy: { id: "desc" },
-  });
+  const tasks = await getTasks();
   return (
     <Suspense>
       <TaskPage tasks={tasks} />
